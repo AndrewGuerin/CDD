8ca1f34668e53e874c52e52d05824499f539816a
AndrewGuerin <c00243040@itcarlow.ie>
2021-10-18 00:42:10 +0100

    Mutex and sync code

---
+++ Lab2/atomicInt/IntegerObj.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lab2.atomicInt;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ *
+ * @author joe
+ */
+class IntegerObj {
+    int value;
+
+    private final AtomicInteger counter = new AtomicInteger(0);
+
+    public int getValue() {
+        return counter.get();
+    }
+
+    IntegerObj(int val) {
+        this.value = val;
+    }
+    int inc(){
+
+        value = getValue();
+        int newValue = value + 1;
+        if(counter.compareAndSet(value, newValue)){
+           return newValue;
+        }
+
+        this.value++;
+        return this.value;
+    }
+}
---
+++ Lab2/mutex/IntegerObj.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lab2.mutex;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ *
+ * @author joe
+ */
+class IntegerObj {
+
+    private ReentrantLock mutex = new ReentrantLock();
+
+    int value;
+
+    IntegerObj(int val) {
+        this.value = val;
+    }
+
+    int inc() {
+        try {
+            mutex.lock();
+            this.value++;
+            return this.value;
+        } finally {
+            mutex.unlock();
+        }
+    }
+}
+
