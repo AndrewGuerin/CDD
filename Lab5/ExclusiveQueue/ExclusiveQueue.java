84722448fc3069e5085137261e721099a3a349d7
AndrewGuerin <c00243040@itcarlow.ie>
2022-01-09 17:55:50 +0000

    Added Dance Method and updated leader and follower sections

--- Lab5/ExclusiveQueue/ExclusiveQueue.java
+++ Lab5/ExclusiveQueue/ExclusiveQueue.java
@@ -1,65 +1,74 @@
 package Lab5.ExclusiveQueue;
-
+/**
+ * Author: Andrew Guerin
+ */
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 
-/** @author Andrew Guerin  */
-
 public class ExclusiveQueue {
-        private final int runtime = 50;
-        private int leaders = followers = 0;
-        private int followers = 0;
-        private final Semaphore mutex = new Semaphore(1);
-        private final Semaphore leaderQueue = new Semaphore(0);
-        private final Semaphore followerQueue = new Semaphore(0);
-        private final Semaphore rendezvous = new Semaphore(0);
+    private final int runtime = 50;
+    private int leaders = followers = 0;
+    private int followers = 0;
+    private final Semaphore mutex = new Semaphore(1);
+    private final Semaphore leaderQueue = new Semaphore(0);
+    private final Semaphore followerQueue = new Semaphore(0);
+    private final Semaphore rendezvous = new Semaphore(0);
+    private ExecutorService executor = Executors.newCachedThreadPool();
 
-        public void leaderFollowerExecution() {
-            for (int index = 0; index < runtime; index++) {
-                final int CurrentIndex = index;
 
-                /** leader section */
-                    public void run() {
-                        mutex.acquireUninterruptibly();
-                        if (followers > 0) {
-                            followers--;
-                            followerQueue.release();
-                        } else {
-                            leaders--;
-                            mutex.release();
-                        }
 
-                        dance("leader " + CurrentIndex);
-                        mutex.release();
-                    }
+    public void leaderFollowerExecution() {
+        for (int index = 0; index < runtime; index++) {
+            final int CurrentIndex = index;
+            /**
+             * Leader Section
+             */
+            executor.submit(() -> {
+                mutex.acquireUninterruptibly();
+                if (followers > 0) {
+                    followers--;
+                    followerQueue.release();
+                } else {
+                    leaders--;
+                    mutex.release();
+                    leaderQueue.acquireUninterruptibly();
+                }
 
-                });
+                dance("leader " + CurrentIndex);
+                rendezvous.acquireUninterruptibly();
+                mutex.release();
+            });
 
-                /** leader section */
-                //follower
+            /**
+             * Follower Section
+             */
+            executor.submit(() -> {
+                mutex.acquireUninterruptibly();
+                if (leaders > 0) {
+                    leaders--;
+                    leaderQueue.release();
+                } else {
+                    followers++;
+                    mutex.release();
+                    followerQueue.acquireUninterruptibly();
+                }
 
-                    public void run() {
-                        if (leaders > 0) {
-                            leaders--;
-                            leaderQueue.release();
-                        } else {
-                            followers++;
-                            mutex.release();
-                        }
-
-                        dance("follower " + CurrentIndex);
-                        rendezvous.release();
-                    }
-
-                });
-            }
-
+                dance("follower " + CurrentIndex);
+                rendezvous.release();
+            });
         }
 
-        private void dance(String type) {
-            System.out.println("Generated " + type);
-        }
     }
 
+    private void dance(String type) {
+        System.out.println("Generated " + type);
+    }
+
+    public static void main(String[] args) {
+        ExclusiveQueue queue = new ExclusiveQueue();
+        queue.leaderFollowerExecution();
+
+    }
+}
